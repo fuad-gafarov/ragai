3c881a7b6c51a405b5f65c8f7ae879a607050f01
fuadgafarov <fuad.t.gafarov@sosial.gov.az>
2025-11-17 11:46:05 +0400

    allowed cors

--- src/api/LLMQueryApi.py
+++ src/api/LLMQueryApi.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 
 from src.model.LLMQueryDto import LLMQueryDto
 from src.service.LLMService import LLMService
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # ← allow all origins
+    allow_credentials=False,  # ← must be False when using "*"
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/query")
 def query(llm_query_dto: LLMQueryDto):
     return LLMService().llm_query(llm_query_dto.text)
